8a5a0c209c46ee7852be84430adcd1e0d076ba2e
dzhudzhi <dzhudzhiev@gmail.com>
2018-09-28 00:30:43 +0300

    advanced zeros: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let multipliers = {}, rest = base;
+    for (let i = 2; i <= rest; i++) {
+        if ((rest % i) == 0) {
+            multipliers[i] ? multipliers[i]++ : multipliers[i] = 1;
+            rest = rest / i;
+            i--;
+        }
+    }
+    let result = 0;
+    for (i in multipliers) {
+        let delimeter = i, sum = 0, power = multipliers[i];
+        while (delimeter <= number) {
+            sum += Math.floor(number / delimeter);
+            delimeter *= i;
+        }
+        sum = Math.floor(sum / power);
+        if (result == 0 || sum < result) {
+            result = sum;
+        } 
+    }
+    return result;
 }
